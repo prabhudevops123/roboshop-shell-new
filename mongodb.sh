bb865ce88a1e60aad72f9510cae2091822085775
Prabhu <prabh.rao@gmail.com>
2025-03-03 17:25:19 +0530

    mongodb changes

--- mongodb.sh
+++ mongodb.sh
@@ -4,7 +4,8 @@ cp mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
 echo -e "\e[33mInstalling MongoDB Server\e[0m"
 dnf install mongodb-org -y &>>/tmp/roboshop.log
 
-# Modify the conf file
+echo -e "\e[33mUpdate mongodb listen address\e[0m"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
 echo -e "\e[33mStart mongoDB server\e[0m"
 systemctl enable mongod &>>/tmp/roboshop.log
