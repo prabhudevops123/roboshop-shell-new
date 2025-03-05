60ace444f0e77f332d45a89ecf16b7ef31f365d0
Prabhu <prabh.rao@gmail.com>
2025-03-05 22:03:16 +0530

    cart

--- redis.sh
+++ redis.sh
@@ -6,7 +6,8 @@ echo -e "\e[33m Install Redis \e[0m"
 dnf install redis -y &>>/tmp/roboshop.log
 
 echo -e "\e[33m Update redis listen address \e[0m"
-sed -i -e 's|127.0.0.1|0.0.0.0|' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf &>>/tmp/roboshop.log
+#sed -i -e 's|127.0.0.1|0.0.0.0|' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf &>>/tmp/roboshop.log
+sed -i -e 's|127.0.0.1|0.0.0.0|' -e /etc/redis.conf etc/redis/redis.conf  /etc/redis/redis.conf &>>/tmp/roboshop.log
 
 echo -e "\e[33m start redis service \e[0m"
 systemctl enable redis &>>/tmp/roboshop.log
