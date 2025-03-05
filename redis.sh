f0c620261b415539206e6b5aed2aa1d4b86e3f35
Prabhu <prabh.rao@gmail.com>
2025-03-05 17:25:51 +0530

    catalogue frontend

---
+++ redis.sh
@@ -0,0 +1,13 @@
+echo -e "\e[33m Enable Redis 7 version \e[0m"
+dnf module disable redis -y &>>/tmp/roboshop.log
+dnf module enable redis:7 -y &>>/tmp/roboshop.log
+
+echo -e "\e[33m Install Redis \e[0m"
+dnf install redis -y &>>/tmp/roboshop.log
+
+echo -e "\e[33m Update redis listen address \e[0m"
+sed -i -e 's|127.0.0.1|0.0.0.0|' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf &>>/tmp/roboshop.log
+
+echo -e "\e[33m start redis service \e[0m"
+systemctl enable redis &>>/tmp/roboshop.log
+systemctl start redis &>>/tmp/roboshop.log
