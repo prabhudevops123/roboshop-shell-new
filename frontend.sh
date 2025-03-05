93f80234c5a221796bc38af19e2fea26647fb3f0
Prabhu <prabh.rao@gmail.com>
2025-03-05 11:17:25 +0530

    catalogue frontend

--- frontend.sh
+++ frontend.sh
@@ -2,17 +2,15 @@ dnf module disable nginx -y
 dnf module enable nginx:1.24 -y
 dnf install nginx -y
 
-cp nginx.conf /etc/nginx/nginx.conf
+#cp nginx.conf /etc/nginx/nginx.conf
+systemctl enable nginx
+systemctl restart nginx
 
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-
 # need to copy config file
-
-systemctl enable nginx
-systemctl restart nginx
 
 
 
