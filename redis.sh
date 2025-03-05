fcbb714950a3e5a7d7a4b1f03a26bdc2c589950a
Prabhu <prabh.rao@gmail.com>
2025-03-05 22:50:11 +0530

    redis

--- redis.sh
+++ redis.sh
@@ -1,5 +1,5 @@
 dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
-dnf module enable redis:remi-6.2 -y
+dnf module enable redis:remi-7 -y
 dnf install redis -y
 sed -i -e 's|127.0.0.1|0.0.0.0|' -e /etc/redis.conf /etc/redis/redis.conf &>>/tmp/roboshop.log
 systemctl enable redis
