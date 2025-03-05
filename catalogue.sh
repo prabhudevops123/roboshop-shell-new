d1144baea51b9a53287eca2be2a4cfa1f7ef1eb8
Prabhu <prabh.rao@gmail.com>
2025-03-05 12:23:46 +0530

    catalogue frontend

--- catalogue.sh
+++ catalogue.sh
@@ -1,10 +1,10 @@
 echo -e "\e[33mConfiguring NodeJS\e[0m"
 #curl -sL https://rpm.nodesource.com/setup_1ts.x | bash &>>/tmp/roboshop.log
-yum module disable nodejs -y #&>>/tmp/roboshop.log
-yum module enable nodejs:20.17 -y #&>>/tmp/roboshop.log
+dnf module disable nodejs -y #&>>/tmp/roboshop.log
+dnf module enable nodejs:20 -y #&>>/tmp/roboshop.log
 
 echo -e "\e[33mInstall NodeJS\e[0m"
-yum install nodejs -y #&>>/tmp/roboshop.log
+dnf install nodejs -y #&>>/tmp/roboshop.log
 
 echo -e "\e[33mAdd App user\e[0m"
 useradd roboshop #&>>/tmp/roboshop.log
@@ -14,29 +14,37 @@ rm -rf /app #&>>/tmp/roboshop.log
 mkdir /app #&>>/tmp/roboshop.log
 
 echo -e "\e[33mDownload App content\e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip #&>>/tmp/roboshop.log
+#curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip #&>>/tmp/roboshop.log
+curl -L -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component-v3.zip
 cd /app
 
 echo -e "\e[33mExtract App Content\e[0m"
-unzip /tmp/catalogue.zip #&>>/tmp/roboshop.log
+#unzip /tmp/catalogue.zip #&>>/tmp/roboshop.log
+unzip /tmp/$component.zip
 cd /app
 
 echo -e "\e[33mInstall NodeJS dependencies\e[0m"
-npm install -g npm@11.1.0 #&>>/tmp/roboshop.log
+npm install #&>>/tmp/roboshop.log
 
 echo -e "\e[33mSetup systemd service\e[0m"
-cp /home/centos/roboshop-shell-new/catalogue.service /etc/systemd/system/catalogue.service #&>>/tmp/roboshop.log
+#cp /home/centos/roboshop-shell-new/catalogue.service /etc/systemd/system/catalogue.service #&>>/tmp/roboshop.log
+cp $pwd/$component.service /etc/systemd/system/$component.service
 
 echo -e "\e[33mStart Catalogue service\e[0m"
 systemctl daemon-reload #&>>/tmp/roboshop.log
-systemctl enable catalogue #&>>/tmp/roboshop.log
-systemctl start catalogue  #&>>/tmp/roboshop.log
+#systemctl enable catalogue #&>>/tmp/roboshop.log
+systemctl enable $component
+#systemctl start catalogue  #&>>/tmp/roboshop.log
+systemctl restart $component
+
 echo -e "\e[33mCopy mongoDB repo file\e[0m"
 cp /home/centos/roboshop-shell-new/mongodb.repo /etc/yum.repos.d/mongodb.repo #&>>/tmp/roboshop.log
 
 echo -e "\e[33mInstalli mongoDB client\e[0m"
-dnf install mongodb-org-shell -y #&>>/tmp/roboshop.log
+#dnf install mongodb-org-shell -y #&>>/tmp/roboshop.log
+dnf install mongodb-mongosh -y
 
 echo -e "\e[33mLoad Schema\e[0m"
-mongo --host mongodb-dev.prabhanjandevops.store </app/schema/catalogue.js #&>>/tmp/roboshop.log
+#mongo --host mongodb-dev.prabhanjandevops.store </app/schema/catalogue.js #&>>/tmp/roboshop.log
+mongosh --host mongodb-dev.prabhanjandevops.store </app/db/master-data.js
 
--- frontend.sh
+++ frontend.sh
@@ -1,17 +1,25 @@
-dnf module disable nginx -y
-dnf module enable nginx:1.24 -y
-dnf install nginx -y
+echo -e "\e[33mInstalling Nginx Server\e[0m"
+dnf module disable nginx -y &>>/tmp/roboshop.log
+dnf module enable nginx:1.24 -y &>>/tmp/roboshop.log
+dnf install nginx -y &>>/tmp/roboshop.log
 
 #cp nginx.conf /etc/nginx/nginx.conf
-systemctl enable nginx
-systemctl restart nginx
+echo -e "\e[33mStarting nginx server\e[0m"
+systemctl enable nginx &>>/tmp/roboshop.log
+systemctl restart nginx &>>/tmp/roboshop.log
 
-rm -rf /usr/share/nginx/html/*
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+echo -e "\e[33mRemoving old app content\e[0m"
+rm -rf /usr/share/nginx/html/* &>>/tmp/roboshop.log
+
+echo -e "\e[33mDownloading frontend content\e[0m"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>/tmp/roboshop.log
+
+echo -e "\e[33mExtract frontend content\e[0m"
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
-# need to copy config file
+unzip /tmp/frontend.zip &>>/tmp/roboshop.log
 
+echo -e "\e[33mFrontend Configuration\e[0m"
+cp /home/centos/roboshop-shell-new/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>/tmp/roboshop.log
 
 
 ##echo -e "${color} Installing Nginx Server ${nocolor}"
