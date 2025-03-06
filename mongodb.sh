5bfe5c1c14a66bff13e2a1ab0d3d3cc40370d1bb
Prabhu <prabh.rao@gmail.com>
2025-03-06 21:34:22 +0530

    mongo

--- mongodb.sh
+++ mongodb.sh
@@ -5,8 +5,8 @@ echo -e "\e[33mInstalling MongoDB Server\e[0m"
 dnf install mongodb-org -y &>>/tmp/roboshop.log
 
 echo -e "\e[33mStart mongoDB server\e[0m"
-systemctl enable mongod &>>/tmp/roboshop.log
-systemctl restart mongod &>>/tmp
+systemctl enable mongodb &>>/tmp/roboshop.log
+systemctl restart mongodb &>>/tmp
 
 echo -e "\e[33mUpdate mongodb listen address\e[0m"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongodb.conf
