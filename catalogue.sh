b0abca77e5475f75805dd7c24b676c28280ee030
Prabhu <prabh.rao@gmail.com>
2025-03-06 23:30:57 +0530

    cata

--- catalogue.sh
+++ catalogue.sh
@@ -33,7 +33,7 @@ systemctl enable catalogue &>>/tmp/roboshop.log
 systemctl start catalogue  &>>/tmp/roboshop.log
 
 echo -e "\e[33mCopy mongoDB repo file\e[0m"
-cp /home/centos /roboshop-shell-new/mongodb.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
+cp /home/centos /roboshop-shell-new/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
 
 echo -e "\e[33mInstalli mongoDB client\e[0m"
 dnf install mongodb-org-shell -y #&>>/tmp/roboshop.log
