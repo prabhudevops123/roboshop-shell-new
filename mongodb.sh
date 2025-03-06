d5b7fe261e04b36637831ff0a8a3110eb6baaee0
Prabhu <prabh.rao@gmail.com>
2025-03-06 21:42:05 +0530

    mongo

--- mongodb.sh
+++ mongodb.sh
@@ -1,5 +1,5 @@
 echo -e "\e[33mCopy mongo DB repo file\e[0m"
-cp /home/centos/roboshop-shell-new/mongo.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
+cp /home/centos/roboshop-shell-new/mongodb.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
 
 echo -e "\e[33mInstalling MongoDB Server\e[0m"
 dnf install mongodb-org -y &>>/tmp/roboshop.log
