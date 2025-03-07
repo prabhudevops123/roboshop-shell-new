3176aae52bc27078d940281acc4a6de6bede1b2d
Prabhu <prabh.rao@gmail.com>
2025-03-07 16:38:35 +0530

    cata

---
+++ mysql.sh
@@ -0,0 +1,6 @@
+dnf module disable mysql -y
+cp /home/centos/roboshop-shell-new/mysql.repo /etc/yum.repos.d/mysql.repo
+dnf install mysql-community-server -y
+systemctl enable mysqld
+systemctl start mysqld
+mysql_secure_installation --set-root-pass RoboShop@1
