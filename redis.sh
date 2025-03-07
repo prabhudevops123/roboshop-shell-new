014d8a337dc5698fa4a24de3a327b78987a09a93
Prabhu <prabh.rao@gmail.com>
2025-03-07 15:14:39 +0530

    cata

--- redis.sh
+++ redis.sh
@@ -7,7 +7,7 @@ dnf install redis -y #&>>/tmp/roboshop.log
 
 echo -e "\e[33m Update redis listen address \e[0m"
 #sed -i 's|127.0.0.1|0.0.0.0|' /etc/redis.conf &>>/tmp/roboshop.log
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf #&>>/tmp/roboshop.log
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf #&>>/tmp/roboshop.log
 
 echo -e "\e[33m start redis service \e[0m"
 systemctl enable redis &>>/tmp/roboshop.log
